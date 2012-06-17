79fd671bef1da329427ce9173e2d6d691febc33f
Chris Strom <eee.c@eeecooks.com>
2012-06-16 23:38:04 -0400

    Bump and normal mapping on the sphere

--- public/assets/procedural-material.js
+++ public/assets/procedural-material.js
@@ -1,8 +1,21 @@
 function proc( options ) {
   return {
+    //color: [80/255, 200/255, 120/255],
+    specular:[1,1,1],
+    shininess: 0.9,
+    env_amount: 1.0,
+    opacity: 1.0,
     textures: {
-      color: '/images/rygb_256.png'
-    },
-    opacity: 1.0
+      color: '/images/rygb_256.png',
+      normal: "/images/cubicvr_js/2576-normal.jpg",
+      bump: "/images/cubicvr_js/2576-bump.jpg",
+      // normal: "/images/line_normal.png",
+      // bump: "/images/line_bump.png",
+      // normal: "/images/crater_normal_map.png",
+      // bump: "/images/crater_bump_map.png",
+      // envsphere: "/images/cubicvr_js/fract_reflections.jpg"
+    }
   };
 }
+
+      // color: "/images/cubicvr_js/2576-diffuse.jpg",
--- public/assets/procedural-sphere-mesh.js
+++ public/assets/procedural-sphere-mesh.js
@@ -9,24 +9,24 @@ function proc( options ) {
       radius: options.size,
       lat: 24,
       lon: 24,
-      material: {
-         // color: [80/255, 200/255, 120/255],
-         // specular:[1,1,1],
-         // shininess: 0.9,
-         // env_amount: 1.0,
-        textures: {
-          color: '/images/rygb_256.png'
-          // color: "/images/cubicvr_js/2576-diffuse.jpg",
-          // normal: "/images/cubicvr_js/2576-normal.jpg",
-          // bump: "/images/cubicvr_js/2576-bump.jpg",
-          // envsphere: "/images/cubicvr_js/fract_reflections.jpg"
-        }
-      },
+      // material: {
+      //    // color: [80/255, 200/255, 120/255],
+      //    // specular:[1,1,1],
+      //    // shininess: 0.9,
+      //    // env_amount: 1.0,
+      //   textures: {
+      //     color: '/images/rygb_256.png'
+      //     // color: "/images/cubicvr_js/2576-diffuse.jpg",
+      //     // normal: "/images/cubicvr_js/2576-normal.jpg",
+      //     // bump: "/images/cubicvr_js/2576-bump.jpg",
+      //     // envsphere: "/images/cubicvr_js/fract_reflections.jpg"
+      //   }
+      // },
       uv: {
         projectionMode: "spherical",
         projectionAxis: "y",
         wrapW: 5,
-        wrapH: 2.5
+        wrapH: 2
       }
     },
     compile: true
